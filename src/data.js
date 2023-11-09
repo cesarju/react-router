5976a75d0e05b7390d56e6bde746ffd8c98df311
Cesar Ticona <cesarju.cc@gmail.com>
2023-11-08 20:46:30 -0400

    add data.js

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const listaObjetos = [
+  {
+    id: 1,
+    titulo: "Objeto 1",
+    descripcion: "Descripción del objeto 1",
+    estado: "Activo",
+  },
+  {
+    id: 2,
+    titulo: "Objeto 2",
+    descripcion: "Descripción del objeto 2",
+    estado: "Inactivo",
+  },
+  {
+    id: 3,
+    titulo: "Objeto 3",
+    descripcion: "Descripción del objeto 3",
+    estado: "Pendiente",
+  },
+  {
+    id: 4,
+    titulo: "Objeto 4",
+    descripcion: "Descripción del objeto 4",
+    estado: "Activo",
+  },
+];
+
+export { listaObjetos };
